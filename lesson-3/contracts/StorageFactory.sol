e3eed8165c76d8f2dd03d6d32106a3a72d847070
Muse-feta <fetamuse@gmail.com>
2025-01-27 12:16:30 +0300

    end of lesson 3

--- lesson-3/contracts/StorageFactory.sol
+++ lesson-3/contracts/StorageFactory.sol
@@ -1,2 +1,29 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
+pragma solidity ^0.8.0;
+import "./SimpleStorage.sol";
+
+contract StorageFactory {
+  SimpleStorage[] public simpleStorageArr;
+
+  function createSimpleStorageContract() public {
+    SimpleStorage simpleStorage = new SimpleStorage();
+    simpleStorageArr.push(simpleStorage);
+  }
+
+  function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+    // in short way
+    // <-- simpleStorageArr[_simpleStorageIndex].store(_simpleStorageNumber); -->
+
+    SimpleStorage simpleStorage = simpleStorageArr[_simpleStorageIndex];
+    simpleStorage.store(_simpleStorageNumber);
+  }
+
+  function sfRetrive(uint256 _simpleStorageIndex) public view returns(uint256) {
+    SimpleStorage simpleStorage = simpleStorageArr[_simpleStorageIndex];
+    return simpleStorage.retrieve();
+    // in short way
+    // return simpleStorageArr[_simpleStorageIndex].retrieve();
+  }
+
+
+}
