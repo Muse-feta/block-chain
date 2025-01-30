438a46d51167b9282f8a9fea7ab4d45a1f207839
Muse-feta <fetamuse@gmail.com>
2025-01-30 18:16:28 +0300

    finished fund contaract

--- lesson-4/contracts/FundMe.sol
+++ lesson-4/contracts/FundMe.sol
@@ -15,10 +15,42 @@ contract FundMe {
     address[] public funders;
     mapping(address => uint256) public adressToFundedAmount;
 
+    address public owner;
+
+    constructor () {
+       owner = msg.sender;
+    }
+
     function fund() public payable {
         require((msg.value.getConversionRate())  > 1e18, "Please enter a value greater than 1 USD");
         funders.push(msg.sender);
         adressToFundedAmount[msg.sender] = msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            adressToFundedAmount[funder] = 0;
+        }
+
+        // reset the array!!
+        funders = new address[](0);
+
+        // we have three ways to send ether in solidty
+        // // 1. transfer()
+        // payable (msg.sender).transfer(address(this).balance);
+        // // 2. send()
+        // bool sendSucess = payable (msg.sender).send(address(this).balance) ;
+        // require(sendSucess, "Transfer failed");
+        // 3. call()
+        (bool callSuccess,) =  payable (msg.sender).call{value: address(this).balance}("") ;
+        require(callSuccess, "call failed");
+        
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only the contract owner can withdraw funds");_;
     }
 
 
